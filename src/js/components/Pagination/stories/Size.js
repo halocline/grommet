8de6dfff8926db1736031bf47aff37acb462c685
Matt Glissmann <mdglissmann@gmail.com>
2021-01-22 12:33:14 -0700

    modify size story

--- src/js/components/Pagination/stories/Size.js
+++ src/js/components/Pagination/stories/Size.js
@@ -8,19 +8,19 @@ export const Size = () => (
     <Box align="start" pad="small" gap="medium">
       <Box>
         <Text>Default</Text>
-        <Pagination numItems={237} />
+        <Pagination numberItems={237} />
       </Box>
       <Box>
         <Text>Small</Text>
-        <Pagination numItems={237} size="small" />
+        <Pagination numberItems={237} size="small" />
       </Box>
       <Box>
         <Text>Medium</Text>
-        <Pagination numItems={237} size="medium" />
+        <Pagination numberItems={237} size="medium" />
       </Box>
       <Box>
         <Text>Large</Text>
-        <Pagination numItems={237} size="large" />
+        <Pagination numberItems={237} size="large" />
       </Box>
     </Box>
   </Grommet>
